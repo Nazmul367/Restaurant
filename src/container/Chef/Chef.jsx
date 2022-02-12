3b1a485f706ea7fe81f41ec9e3662e87e0cf2662
nazmul367 <najmulrizvy@gmail.com>
2022-02-12 20:30:31 +0600

    finished

---
+++ src/container/Chef/Chef.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { SubHeading } from '../../components'
+
+import { images } from '../../constants'
+import './Chef.css';
+
+const Chef = () => (
+  <div className='app__bg app__wrapper section__padding'>
+    <div className='app__wrapper_img app__wrapper_img-reverse'>
+      <img src={images.chef} alt="chef" />
+    </div>
+
+    <div className='app__wrapper_info'>
+      <SubHeading title="Chef's World" />
+      <h1 className='headtext__cormorant'>What we believe in</h1>
+
+      <div className='app__chef-content'>
+        <div className='app__chef-content_quote'>
+          <img src={images.quote} alt="quote" />
+          <p className='p__opensans'>There are many variations of passages of Lorem Ipsum available</p>
+        </div>
+
+        <p className='p__opensans'>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur,</p>
+      </div>
+
+      <div className='app__chef-sign'>
+        <p>Emrul Shibly</p>
+        <p className='p__opensans'>Chef & Founder</p>
+        <img src={images.sign} alt="sign" />
+      </div>
+    </div>
+  </div>
+);
+
+export default Chef;
